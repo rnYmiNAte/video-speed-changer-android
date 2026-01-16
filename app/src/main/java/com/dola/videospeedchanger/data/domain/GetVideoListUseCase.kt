03be38138eb3789b9b0c6df786ea9badba6de51e
rnYmiNAte <167747913+rnYmiNAte@users.noreply.github.com>
2026-01-16 18:46:04 +0800

    Update GetVideoListUseCase.kt

--- app/src/main/java/com/dola/videospeedchanger/data/domain/GetVideoListUseCase.kt
+++ app/src/main/java/com/dola/videospeedchanger/data/domain/GetVideoListUseCase.kt
@@ -1,1 +1,10 @@
-0
+package com.dola.videospeedchanger.domain
+
+import com.dola.videospeedchanger.data.model.VideoItem
+import com.dola.videospeedchanger.data.repository.VideoRepository
+
+class GetVideoListUseCase(private val repository: VideoRepository) {
+    operator fun invoke(): List<VideoItem> {
+        return repository.getVideoList()
+    }
+}
