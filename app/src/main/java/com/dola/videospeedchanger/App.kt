6340fa9e259956f709f25e9bbe0a5de7bd399ca2
rnYmiNAte <167747913+rnYmiNAte@users.noreply.github.com>
2026-01-16 18:44:45 +0800

    Update App.kt

--- app/src/main/java/com/dola/videospeedchanger/App.kt
+++ app/src/main/java/com/dola/videospeedchanger/App.kt
@@ -1,1 +1,9 @@
-0
+package com.dola.videospeedchanger
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application() {
+    // Hilt auto-initializes components here
+}
