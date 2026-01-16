ad323a2d80f44d6cb217028d6ed14e8170ea229e
rnYmiNAte <167747913+rnYmiNAte@users.noreply.github.com>
2026-01-16 18:47:59 +0800

    Update Videoltem.kt

--- app/src/main/java/com/dola/videospeedchanger/data/model/Videoltem.kt
+++ app/src/main/java/com/dola/videospeedchanger/data/model/Videoltem.kt
@@ -1,1 +1,13 @@
-0
+package com.dola.videospeedchanger.data.model
+
+import java.io.File
+import java.math.BigDecimal
+
+data class VideoItem(
+    val id: String, // Unique identifier (e.g., media store ID)
+    val file: File,
+    val displayName: String,
+    val duration: BigDecimal, // Total duration in seconds (high-precision)
+    val size: Long, // File size in bytes
+    val mimeType: String
+)
